d8f6bca9c8e26919097bfc0d051d55bdae627eee
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-10-18 20:02:36 -0500

    feat: local to prod environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  //msManager: 'https://ms-manager-1099250987692.us-central1.run.app/api',
-  msManager: 'http://localhost:9092/api',
+  msManager: 'https://ms-manager-1099250987692.us-central1.run.app/api',
+  //msManager: 'http://localhost:9092/api',
 
   //msAuth: 'http://localhost:9090/api',
-  msUsers: 'http://localhost:9091/api',
+  //msUsers: 'http://localhost:9091/api',
   msAuth: 'https://ms-auth-1099250987692.us-central1.run.app/api',
-  //msUsers: 'https://ms-users-1099250987692.us-central1.run.app/api'
+  msUsers: 'https://ms-users-1099250987692.us-central1.run.app/api'
 };
