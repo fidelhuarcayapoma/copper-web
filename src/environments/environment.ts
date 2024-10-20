7a17f586a4b2947368dec442347251f96efb6263
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2024-10-20 12:58:42 -0500

    feat: env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //msManager: 'https://ms-manager-1099250987692.us-central1.run.app/api',
-  msManager: 'http://localhost:9092/api',
+  msManager: 'https://ms-manager-1099250987692.us-central1.run.app/api',
+  //msManager: 'http://localhost:9092/api',
 
   //msAuth: 'http://localhost:9090/api',
   //msUsers: 'http://localhost:9091/api',
