e86686161df00db06cd38c1badf754c9436f1190
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2025-01-09 18:39:32 -0500

    feat: update environment API URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  msManager: 'https://ms-manager-1099250987692.us-central1.run.app/api',
+  msManager: 'https://ms-manager-43702223192.us-central1.run.app/api',
   //msManager: 'http://localhost:9092/api',
 
   //msAuth: 'http://localhost:9090/api',
   //msUsers: 'http://localhost:9091/api',
-  msAuth: 'https://ms-auth-1099250987692.us-central1.run.app/api',
-  msUsers: 'https://ms-users-1099250987692.us-central1.run.app/api'
+  msAuth: 'https://ms-auth-43702223192.us-central1.run.app/api',
+  msUsers: 'https://ms-users-43702223192.us-central1.run.app/api'
 };
